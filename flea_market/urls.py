4ef630dab359e0f017e80c267ad17ead13c457db
zhanhailiu <you@example.com>
2018-05-22 11:07:36 +0800

    rectify indention

--- flea_market/urls.py
+++ flea_market/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', include('market.urls')),
-    url(r'^market/', include('market.urls')),
+    url(r'^admin/',     admin.site.urls),
+    url(r'^$',          include('market.urls')),
+    url(r'^market/',    include('market.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
