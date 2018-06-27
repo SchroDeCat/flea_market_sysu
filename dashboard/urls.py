00d9f618903fc765527a1ffbad905945d41b8a10
SkyCat <schrodecat@outlook.com>
2018-06-27 13:10:13 +0800

    add bulletin to side bar

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,7 @@ from dashboard import views
 urlpatterns = [
     # The home page
     url(r'^$', views.index, name='dashboard'),
+    url(r'index', views.index, name='index'),
+    url(r'bulletin', views.index, name='bulletin'),
+
 ]
