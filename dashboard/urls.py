54de2ba69e132dc6de7d1cea729fee23fa5cbd82
SkyCat <schrodecat@outlook.com>
2018-06-27 13:29:40 +0800

    fix severl bugs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,6 @@ from dashboard import views
 urlpatterns = [
     # The home page
     url(r'^$', views.index, name='dashboard'),
-    url(r'index', views.index, name='index'),
     url(r'bulletin', views.index, name='bulletin'),
 
 ]
